aa4200941e680eaf0d5ec4171538ec745b5fe088
lieuzhenghong <lieuzhenghong@gmail.com>
2015-01-24 21:52:02 +0800

    5th commit

--- quiz_maker.js
+++ quiz_maker.js
@@ -11,27 +11,30 @@ function Option(op_no, t_val, q_no){
 	this.question_number = q_no;
 	this.onclick = function(t_val){
 		return t_val;
-		if (t_val == false) {
+		if (t_val === false) {
 			alert("Wrong!");
 		}
 		else {
 			alert("Right!");
-		}
+		};
 	}
 }
 
 function createNewOption(op_name, op_no, t_val, q_no){
-	op_name = new Option(op_no, t_val, q_no);
+	
+	//We use op_name here as the name of the new created option
+	new_option = new Option(op_no, t_val, q_no);
 
-	console.log(option_name);
 	console.log(op_name);
+	console.log(new_option);
 
 	var option_html = document.createElement("div");
 	option_html.id = op_name;
 
 	var parent = document.getElementById('question');
 	parent.appendChild(option_html);
-	document.getElementById(op_name).innerHTML = option;
+	//console.log("The parent element"+ parent);
+	document.getElementById(op_name).innerHTML = OPTION_CONTENT;
 }
 
 /* -----------------------
@@ -39,79 +42,69 @@ function createNewOption(op_name, op_no, t_val, q_no){
 --------------------------- */
 
 	i = 0;
-	global_quiz_name = "";
+	QUIZ_NAME = "";
 
 	function callXml(quiz_name){
-		global_quiz_name = quiz_name;
+		QUIZ_NAME = quiz_name;
 		xmlhttp=new XMLHttpRequest();
 		xmlhttp.open("GET",'./Quiz Questions/' + quiz_name,false);
 		xmlhttp.send();
 		xmlDoc=xmlhttp.responseXML;	
 		//Get every single element with tag name 'question'
 		questionArray = xmlDoc.getElementsByTagName("question");	
-		console.log(questionArray);		
-		displayQuestion(questionArray);
+		console.log(questionArray);
+
+		//This calls a function to grab all the data for ONE question		
+		getDataOfOneQnFrom(questionArray);
 		}
 
 //---------------------------------
 /* TAKING FROM THE XML FILE */
 //-----------------------------------------
 		
-		function displayQuestion(questionArray){
-			header=(questionArray[i].getElementsByTagName("header")[0].childNodes[0].nodeValue);
-			content=(questionArray[i].getElementsByTagName("content")[0].childNodes[0].nodeValue);
-			options=(questionArray[i].getElementsByTagName("option"));
+		function getDataOfOneQnFrom(questionArray){
+			HEADER=(questionArray[i].getElementsByTagName("header")[0].childNodes[0].nodeValue);
+			CONTENT=(questionArray[i].getElementsByTagName("content")[0].childNodes[0].nodeValue);
+			OPTIONS=(questionArray[i].getElementsByTagName("option"));
 			
 			//get as many options as there are in that particular question
 
-			optionsArray = [];
+			OPTIONSARRAY = [];
+			//The following function transfers nodelist to array
+			intermediateArray();
+		}
 
-			for (a = 0; a < options.length; a++){
-				//We push the corresponding option into an options Array
-				//just to make it simpler
-				//The options array ONLY contains options from ONE question
-				option = options[a].childNodes[0].nodeValue;
+		function intermediateArray() {
+			//We push ALL options' content into an options Array
+			//just to make it simpler
+			//The options array ONLY contains options from ONE question
+
+			for (a = 0; a < OPTIONS.length; a++){
+				OPTION_CONTENT = OPTIONS[a].childNodes[0].nodeValue;
 				//option.truth_value =  options[a].getAttributeNode('truth_value');
-				console.log(option);
-				optionsArray.push(option);
-			}
+				console.log(OPTION_CONTENT);
+				OPTIONSARRAY.push(OPTION_CONTENT);
+				displayInfo();			
+		}
+	}
 
-				// This block of code generates the HTML elements dynamically
-				//For header and content, they are fixed, so we can just add in
-				document.getElementById("header").innerHTML=header;	
-				document.getElementById("content").innerHTML=content;
+		function displayInfo(){
 
-				//For option, there is no option divs, we have to create ourselves
-				//So, let's call a createNewOption function.
+			// This block of code generates the HTML elements dynamically
+			//For header and content, they are fixed, so we can just add in
+			document.getElementById("header").innerHTML=HEADER;	
+			document.getElementById("content").innerHTML=CONTENT;
 
-				for (j = 0; j < optionsArray.length; j++){
-					option_name = i + "_" + j;
-					option_number = j;
-					question_number = Number(i);
-					//truth_value = optionsArray[j].getAttribute('truth_value');
-					truth_value = false;
-					//Finally, we create a new Option.
-					createNewOption(option_name, option_number, question_number, truth_value);
+			//For option, there is no option divs, we have to create ourselves
+			//So, let's call a createNewOption function.
+
+			for (j = 0; j < OPTIONSARRAY.length; j++){
+				OPTION_NAME = i + "_" + j;
+				OPTION_NUMBER = j;
+				QUESTION_NUMBER = Number(i);
+				//truth_value = OPTIONSARRAY[j].getAttribute('truth_value');
+				TRUTH_VALUE = false;
+				//Finally, we create a new Option.
+				createNewOption(OPTION_NAME, OPTION_NUMBER, QUESTION_NUMBER, TRUTH_VALUE);
 				}
-			}
-
-/*
-		function left(){
-			if (i>0){
-			i--;
-			displayQuestion();
-			}
-		}		
-		
-		function right(){
-			if (i<(questionArray.length-1)){
-				i++;
-				callXml(global_quiz_name);
-				}
-			else {
-				endQuiz();
-				document.getElementById("question").innerHTML="Well done! You have finished all the questions!";				
-			}
-				
-			}
-			*/
+		}
