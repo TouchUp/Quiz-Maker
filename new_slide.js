8983684b159c5dc2eb5063a2ea684ccb5a23d67f
lieuzhenghong <lieuzhenghong@gmail.com>
2015-04-03 19:34:24 +0800

    +++

--- new_slide.js
+++ new_slide.js
@@ -1,16 +1,27 @@
-function createNewSlide() {
-	var new_slide = document.createElement('div');
-	document.body.appendChild(new_slide);
-	new_slide.className = 'slide';
-	/** this slide has a unique ID **/
-	//new_slide.id = 
-	new_slide.innerHTML += "<form>ID: <input type = 'number'></input> Links to: <input type = 'number'></input></form>";
-	//Add new button to slide
-	new_slide.innerHTML += '<button onclick = "createNewOption()">Create New Option </button>';
+var counter = 0;
+var slide_array = [];
+
+function createNewSlide(no) {
+	for (i = 0; i < no; i++) {
+		var new_slide = document.createElement('div');
+		document.getElementById('slide_list').appendChild(new_slide);
+		new_slide.className = 'slide';
+		/** this slide has a unique ID **/
+		new_slide.id = "slide_" + counter;
+
+		console.log(new_slide.id);
+		new_slide.innerHTML += "<form>ID: " +new_slide.id + "<br> Links to: <input type = 'number'></input><br>Body Text: <input type = 'text'></input></form>";
+		//Add new button to slide
+		new_slide.innerHTML += '<button onclick = "createNewOption('+counter+')">Create New Option </button>';
+
+		counter++;		
+	}
 }
 
-function createNewOption(){
+function createNewOption(id){
 	var new_option = document.createElement('div');
-	new_slide.appendChild(new_option);
-	new_option.innerHTML += '<h2>Option 1</h2>';	
+	var slide_selected = document.getElementById('slide_'+id);
+	slide_selected.appendChild(new_option);
+	new_option.innerHTML += '<h2>Option</h2>';
+	new_option.innerHTML += '<form><select><option value = "img">Image</option><option value = "text">Text</option><option value = "code">Code</option></select><input type = "text"></input></form>';
 }
